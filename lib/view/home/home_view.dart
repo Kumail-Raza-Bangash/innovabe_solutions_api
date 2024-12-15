a3bfd8bfdb3e5c9474149acc9ff1d27e1ee2e14e
Kumail Raza <kumailraza260@gmail.com>
2024-12-15 19:03:04 +0500

    UPDATED CODE GET API

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -82,10 +82,50 @@ class _HomeViewState extends State<HomeView> {
               itemCount: todoListVM.todoList.length,
               itemBuilder: (context, index) {
                 final todo = todoListVM.todoList[index];
-                return ListTile(
-                  title: Text(todo.id?.toString() ?? 'No ID'),
-                  subtitle: Text(todo.value ?? 'No Value'),
-                  trailing: Text(todo.status ?? 'No Status'),
+                return Card(
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(
+                      vertical: Dimensions.height10,
+                      horizontal: Dimensions.width15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(Dimensions.radius15),
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: AppColor.primaryColor,
+                      child: Text(
+                        todo.id?.toString() ?? 'N/A',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    title: Text(
+                      todo.value ?? 'No Value',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Dimensions.font16,
+                        color: AppColor.blackColor,
+                      ),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: Dimensions.height10 / 2),
+                        Text(
+                          'Status: ${todo.status ?? 'No Status'}',
+                          style: TextStyle(
+                            fontSize: Dimensions.font16,
+                            color: AppColor.greyColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColor.primaryColor,
+                      size: 16,
+                    ),
+                    onTap: () {},
+                  ),
                 );
               },
             );
